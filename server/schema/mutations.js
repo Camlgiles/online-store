2fa17cb37911e4d220e205edea7e088a6f8ef082
Camlgiles <cgiles@gilescpa.com>
2019-08-28 11:04:14 -0700

    add logout functionality

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -3,8 +3,10 @@ const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
 const mongoose = require("mongoose");
 const CategoryType = require("./types/category_type");
 const ProductType = require("./types/product_type");
+const UserType = require("./types/user_type");
 const Category = mongoose.model("categories");
 const Product = mongoose.model("products");
+const AuthService = require("../services/auth")
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -45,10 +47,17 @@ const mutation = new GraphQLObjectType({
       }
     },
 
-    // updateProductCategory: {
-
-    // }
-
+    register: {
+      type: UserType,
+      args: {
+          name: { type: GraphQLString },
+          email: { type: GraphQLString },
+          password: { type: GraphQLString }
+      },
+      resolve(_, args) {
+          return AuthService.register(args);
+      }
+  }
   }
 });
 
---
+++ server/validation/login.js
@@ -0,0 +1,24 @@
+const validText = require("./valid-text");
+const Validator = require("validator");
+
+module.exports = function validateLoginInput(data) {
+  data.email = validText(data.email) ? data.email : "";
+  data.password = validText(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    return { message: "Email is invalid", isValid: false };
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    return { message: "Email field is required", isValid: false };
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    return { message: "Password field is required", isValid: false };
+  }
+
+  return {
+    message: "",
+    isValid: true
+  };
+};
